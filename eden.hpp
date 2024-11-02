3624c676e6ebcdbe7b56d1c7e5da66a421cb5d12
Rylan Young <Rylan.M.Young@gmail.com>
2024-11-02 09:14:54 -0600

    Fixed string names

--- eden.hpp
+++ eden.hpp
@@ -27,14 +27,14 @@ class display3DEN
                 
                 class E2TBMissionTerrainBuilderEnh
                 {
-                    text   = "STR_SCAR_E2TB_3DEN_MenuBarEnh";
+                    text   = "$STR_SCAR_E2TB_3DEN_MenuBarEnh";
                     data   = "E2TBMissionTerrainBuilder";
                     action = "[false,true] spawn SCAR_E2TB_fnc_exportFrom3DEN;";
                 };
 
                 class E2TBMissionTerrainBuilderEnhAbsolute
                 {
-                    text   = "STR_SCAR_E2TB_3DEN_MenuBarEnhAbsolute";
+                    text   = "$STR_SCAR_E2TB_3DEN_MenuBarEnhAbsolute";
                     data   = "E2TBMissionTerrainBuilder";
                     action = "[true,true] spawn SCAR_E2TB_fnc_exportFrom3DEN;";
                 };
